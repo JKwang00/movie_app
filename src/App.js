da48522a5ea04276457df7b04a0c73e8a0fa271e
my name <you@abc.com>
2019-09-25 10:30:49 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Potato({name}) {
+  return (
+    <div>I love Potato{name}</div>
+  )
+}
+
+const datas = [
+  {
+    name: "홍길동"
+  },
+  {
+    name: "강감찬"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      {datas.map( current => (
+        <Potato name="apple" />
+      ))}
     </div>
   );
 }
