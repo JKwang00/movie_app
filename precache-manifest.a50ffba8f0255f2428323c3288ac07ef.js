3dd05a5dd888b0c1bf340e5bf8c7ee652a2fa3b4
my name <you@abc.com>
2019-10-07 15:01:52 +0900

    Updates

---
+++ precache-manifest.a50ffba8f0255f2428323c3288ac07ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6012e1be5f52d94ff39c0f412270930",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e25d5f10f0d309f54f54",
+    "url": "/movie_app/static/css/main.c698557a.chunk.css"
+  },
+  {
+    "revision": "dee1dfeb0ee4e5d3fc91",
+    "url": "/movie_app/static/js/2.a9a352c0.chunk.js"
+  },
+  {
+    "revision": "e25d5f10f0d309f54f54",
+    "url": "/movie_app/static/js/main.9d36b0e1.chunk.js"
+  },
+  {
+    "revision": "a3a26237de1de1d250ab",
+    "url": "/movie_app/static/js/runtime-main.ebbd9dda.js"
+  }
+]);
